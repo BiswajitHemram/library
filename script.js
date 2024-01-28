ec7c5d6581e128b74bf567f871cec7ad8343c7de
BiswajitHemram <biswajithemram753@gmail.com>
2024-01-28 10:55:04 +0530

    date object to footer copywrite

---
+++ script.js
@@ -0,0 +1,5 @@
+// date object footer copywrite
+const copyrightDate = document.querySelector("#copyrightDate")
+const date = new Date()
+const year = date.getFullYear()
+copyrightDate.textContent = year
